149527998405206b8dac6dff871187af2a60da9f
Umar Bello Kanwa <umarbellokanwa@gmail.com>
2025-12-17 14:45:12 +0000

    Make it general

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
-from schema import EmailMessage
-from service import send_mail
 from fastapi.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from schema import SendEmailRequest
 
 templates = Jinja2Templates(directory="templates")
 
@@ -14,8 +14,17 @@ async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/send/email")
-async def send_email_route(email_message: EmailMessage):
-    return await send_mail(email_message)
+async def send(payload: SendEmailRequest):
+    config = ConnectionConfig(**payload.config.model_dump())
+    message = MessageSchema(**payload.message.model_dump())
+
+    fm = FastMail(config)
+    await fm.send_message(message)
+
+    return {
+        "message": "Email has been sent",
+        "recipients": payload.message.recipients
+    }
 
 if __name__ == "__main__":
     import uvicorn
--- schema.py
+++ schema.py
@@ -1,22 +1,22 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
-
-class Sender(BaseModel):
-    username: EmailStr
-    mail_from: EmailStr
-    password: str
-    port: int
-    server: str
-    ssl_tls: bool
-    starttls: bool
-
-
-class Receiver(BaseModel):
-    template: str
-    email: EmailStr
+class CustomMessageSchema(BaseModel):
+    recipients: list[EmailStr]
     subject: str
+    subtype: Optional[str] = None
+    body: str | None = None
 
+class CustomConnectionConfig(BaseModel):
+    MAIL_USERNAME: str
+    MAIL_FROM: str
+    MAIL_PASSWORD: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_SSL_TLS: bool
+    MAIL_STARTTLS: bool
 
-class EmailMessage(BaseModel):
-    sender: Sender
-    receiver: Receiver
+class SendEmailRequest(BaseModel):
+    config: CustomConnectionConfig
+    message: CustomMessageSchema
+
